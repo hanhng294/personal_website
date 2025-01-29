bffddaeee58613099bc031c6f2cac36fee1ace77
Grace Nguyen <103975253+hanhng294@users.noreply.github.com>
2025-01-29 23:13:19 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function scrollToAbout() {
+    document.getElementById("about-section").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToProject() {
+    document.getElementById("project-section").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToWork() {
+    document.getElementById("work-section").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToActivity() {
+    document.getElementById("activities-section").scrollIntoView({ behavior: "smooth" });
+}
+function scrollToContact() {
+    document.getElementById("contact-section").scrollIntoView({ behavior: "smooth" });
+}
